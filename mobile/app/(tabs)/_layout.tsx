e993471d9acb7835d86da473a428df5ff8af9a95
Anthony <anthonysalisbury98@outlook.com>
2025-08-15 10:57:08 +0100

    Set up React Native project with Expo and NativeWind

---
+++ mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,33 @@
+import { IconSymbol } from "@/app-example/components/ui/IconSymbol.ios";
+import TabBarBackground from "@/app-example/components/ui/TabBarBackground";
+import { HapticTab } from "@/components/HapticTap";
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#000",
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            position: 'absolute',
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+    </Tabs>
+  )
+}
---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500 text-3xl font-bold">PocketDex</Text>
+    </SafeAreaView>
+  );
+}
