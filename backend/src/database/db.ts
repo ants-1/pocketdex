d67d29681576a7a79b874d0aebb2702b1ddf69bb
Anthony <anthonysalisbury98@outlook.com>
2025-08-19 09:10:35 +0100

    Fix typo preventing database from loading

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,9 +1,11 @@
 import { Pool } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const db = new Pool({
   user: process.env.DB_USER,
   host: "localhost",
-  database: process.env.DATABASE,
+  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 })
